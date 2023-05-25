0f146dcaea315a62293e76ba318b53b55f29646c
mohammedzarhbouch <120816@vistacollege.nl>
2023-05-25 10:01:54 +0200

    backend map

---
+++ back-end/ch11.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `agenda`(
+    `agenda_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT NOT NULL,
+    `date` BIGINT NOT NULL,
+    `time` BIGINT NOT NULL
+);
+ALTER TABLE
+    `agenda` ADD PRIMARY KEY(`agenda_id`);
+CREATE TABLE `pills`(
+    `pill_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `pill-name` VARCHAR(255) NOT NULL,
+    `pill_amount` BIGINT NOT NULL,
+    `description` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `pills` ADD PRIMARY KEY(`pill_id`);
+CREATE TABLE `notities`(
+    `notitie_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` BIGINT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `date` DATE NOT NULL
+);
+ALTER TABLE
+    `notities` ADD PRIMARY KEY(`notitie_id`);
+CREATE TABLE `users`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `users` ADD PRIMARY KEY(`id`);
+ALTER TABLE
+    `notities` ADD CONSTRAINT `notities_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE
+    `agenda` ADD CONSTRAINT `agenda_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE
+    `pills` ADD CONSTRAINT `pills_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
