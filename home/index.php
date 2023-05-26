db894eaacbd4989d8190a74afb8290e5a03b2dd7
Kevinsimon046 <511027@vistacollege.nl>
2023-05-26 12:39:51 +0200

    added pic and text and footer to hompage

--- home/index.php
+++ home/index.php
@@ -20,7 +20,26 @@
         <form action="../notitie/notitie.php" method="POST">
         <input type="submit" value="notitie">
         </form>
-        <?php
+
+        <container>
+        <div>
+            <img src="../Foto/Blije-ouderen-canva.jpg" id="fotohome">
+        </div>
+            <div id="tekst">We begrijpen dat ouderen vaak extra ondersteuning
+                en zorg nodig hebben en we zijn er om te helpen. Onze missie is 
+                om een betere kwaliteit van leven te bieden aan ouderen door 
+                middel van verschillende vormen van hulp en ondersteuning. 
+                We bieden praktische hulp, zoals het doen van boodschappen, 
+                koken en het helpen bij huishoudelijke taken. Daarnaast zorgen 
+                we ook voor emotionele ondersteuning door middel van gezelschap,
+                luisteren naar hun verhalen en het bieden van een sociaal netwerk.
+                We streven ernaar om ouderen te helpen hun onafhankelijkheid te
+                behouden en te genieten van een gelukkig en vervuld leven.</div>
+    </container>
+
+    <div id="box1">.</div>
+
+    <?php
         session_start();
 
         if (isset($_SESSION['id'])) {
@@ -33,5 +52,86 @@
         }
         
         ?>
+  
+        <!-- dit is de footer -->
+        <main>
+        
+        </main>
+        <footer>
+            <div id="footer_content">
+                <div id="footer_contacts">
+                    <p>Leef Vitaal, Kies Zorgvitaal</p>
+    
+                    <div id="footer_social_media">
+                        <a href="#" class="footer-link" id="instagram">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+    
+                        <a href="#" class="footer-link" id="facebook">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a>
+    
+                        <a href="#" class="footer-link" id="whatsapp">
+                            <i class="fa-brands fa-whatsapp"></i>
+                        </a>
+                    </div>
+                </div>
+                
+                <ul class="footer-list">
+                    <li>
+                        <h3>HANDIGE LINKS</h3>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Over Ons</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Evenementen</a>
+                    </li>
+                </ul>
+    
+                <ul class="footer-list">
+                    <li>
+                        <h3>PAGINA'S</h3>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Mijn Pillen</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Artikelen</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Notities</a>
+                    </li>
+                    <li>
+                        <a href="#" class="footer-link">Agenda</a>
+                    </li>
+                </ul>
+    
+                <div id="footer_subscribe">
+                    <h3>ABONNEER</h3>
+    
+                    <p>
+                        vul hier jouw e-mail in om op de hoogte te blijven 
+                        van ons laatste nieuws!
+                    </p>
+    
+                    <div id="input_group">
+                        <input type="email" id="email">
+                        <button>
+                            <i class="fa-regular fa-envelope"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+    
+            <div id="footer_copyright">
+                &#169
+                2023 Leef Vitaal, Kies Zorgvitaal
+            </div>
+        </footer>
+
 </body>
 </html>
